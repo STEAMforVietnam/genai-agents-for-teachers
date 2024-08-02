ebf73992c572a57f347e1af9b380dde4052a9ea7
Huy Nguyen <nguyenhoaihuy92@gmail.com>
2024-08-01 22:41:15 -0700

    move prompt list to crew agents

--- agents/exam.py
+++ agents/exam.py
@@ -6,13 +6,15 @@ from agents.custom_tools import create_exam_html_maker_tool
 from crewai_tools.tools.json_search_tool.json_search_tool import JSONSearchTool
 from crewai_tools.tools.file_read_tool.file_read_tool import FileReadTool
 from .models import ExamJSON
+from agents.prompt_list import PromptList
 
 
 class ExamCrew(CustomCrew):
-    def __init__(self, creator_prompt,
-                 orchestrator_prompt=None,
-                 checker_prompt=None,
-                 html_creator_prompt=None):
+    def __init__(self, 
+                 creator_prompt=PromptList('./prompts/prompt_list_test_creator.txt'),
+                 orchestrator_prompt=PromptList('./prompts/prompt_list_test_orchestrator.txt'),
+                 checker_prompt=PromptList('./prompts/prompt_list_test_checker.txt'),
+                 html_creator_prompt=PromptList('./prompts/prompt_list_test_html_creator.txt')):
         super(ExamCrew, self).__init__(
             creator_prompt, orchestrator_prompt,
             checker_prompt, html_creator_prompt)
@@ -21,7 +23,7 @@ class ExamCrew(CustomCrew):
     def _get_tools(self):
         super()._get_tools()
         ### ADD JSONParserTool
-        self.tools.append(JSONSearchTool('./output/matrix.json')) 
+        self.tools.append(JSONSearchTool('./outputs/matrix.json')) 
 
         ### ADD HTMLMakerTool
         self.tools.append(create_exam_html_maker_tool)
@@ -110,7 +112,7 @@ class ExamCrew(CustomCrew):
             expected_output=test_orchestrator_task_expected_output,
             agent=agent,
             output_file="dulieu-de-thi.md",
-            tools=[FileReadTool('./output/matrix.json')]
+            tools=[FileReadTool('./outputs/matrix.json')]
         )
 
     def _create_exam_generator_task(self, agent):
@@ -150,7 +152,7 @@ class ExamCrew(CustomCrew):
 
     def run(self, inputs=None):
         
-        with open("./output/matrix.json", "r", encoding="utf-8") as file:
+        with open("./outputs/matrix.json", "r", encoding="utf-8") as file:
             matrix = json.load(file)
         inputs = {"ma_tran_de_bai": matrix}
         return super().run()
